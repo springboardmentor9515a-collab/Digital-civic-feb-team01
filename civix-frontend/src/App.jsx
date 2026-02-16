5379c3e03e6976291f1bdeff41c3fbf73f82451a
nkdas7582 <fragrancenishantad24@gmail.com>
2026-02-16 19:26:11 +0530

    add dashboard page

--- civix-frontend/src/App.jsx
+++ civix-frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-
+import Dashboard from "./pages/Dashboard"
 function App() {
   return (
     <Router>
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
